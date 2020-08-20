2926edae88c6e416a77b45aa2b580b8b9cadf42f
MOHAMMAD MIZAN <mohammad25524143@gmail.com>
2020-08-20 22:39:51 +0600

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+///////////////skill-part//////////////////////////////
+$('.html').LineProgressbar({
+    percentage: 98,
+    fillBackgroundColor: '#1EB0ED',
+    height: '10px',
+    radius: '0px'
+});
+$('.css').LineProgressbar({
+    percentage: 98,
+    fillBackgroundColor: '#1EB0ED',
+    height: '10px',
+    radius: '0px'
+});
+$('.js').LineProgressbar({
+    percentage: 85,
+    fillBackgroundColor: '#1EB0ED',
+    height: '10px',
+    radius: '0px'
+});
+$('.php').LineProgressbar({
+    percentage: 90,
+    fillBackgroundColor: '#1EB0ED',
+    height: '10px',
+    radius: '0px'
+});
+
+$('.laravel').LineProgressbar({
+    percentage: 95,
+    fillBackgroundColor: '#1EB0ED',
+    height: '10px',
+    radius: '0px'
+});
